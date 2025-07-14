76077cc88677e759c86fcc432c6b7778993146ae
Vikashmaurya01 <mauryavikas4665@gmail.com>
2025-07-14 11:39:09 +0530

    Design Change

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import AboutSection from "@/components/AboutSection";
 import { HeroSection } from "@/components/HeroSection";
+import TourType from "@/components/TourType";
+import WhyChoose from "@/components/WhyChooses";
 
 export default function Home() {
   return (
     <div className="min-h-screen w-full bg-white">
       <HeroSection />
       <AboutSection />
+      <TourType />
+      <WhyChoose />
     </div>
   );
 }
